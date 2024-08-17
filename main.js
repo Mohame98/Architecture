1e41c38ca0463d7a958160988d9a72d78216b379
mohame98 <wtafroach@gmail.com>
2024-08-17 02:19:11 -0400

    js code

---
+++ main.js
@@ -0,0 +1,23 @@
+// $('.slider-holder').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     dots: true,
+//     // autoplay: true,
+//     autoplaySpeed: 2000,
+//     speed: 400,
+//     ease: 'linear',
+//     centerMode: true,
+//     centerPadding: $('.slick').find('.item').outerWidth() / 2
+// });
+
+// $('#btn1').on('click', function(){
+//     $('h2').text('is a test').css('color', 'red');
+//     $('h3').toggleClass('subtitle');
+// });
+
+// $('.question').on('click', function(){
+//     $('.question').toggleClass('is-open');
+
+//     $('.answer').slideToggle();
+// });
